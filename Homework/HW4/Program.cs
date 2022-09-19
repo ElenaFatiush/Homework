18915796efbf9f9e6d18a71c3da8a5c3263b98b4
Elena <helen.ushenko@gmail.com>
2022-09-19 14:34:21 +0300

    Последняя задача

---
+++ Homework/HW4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+Console.WriteLine("Введите число N");
+int a = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= a; i++)
+        if (i % 2 == 0)
+            Console.WriteLine(i);
