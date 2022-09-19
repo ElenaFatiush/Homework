393265818067f9497924bd6b9597d79d5db6edfd
Elena <helen.ushenko@gmail.com>
2022-09-19 13:51:29 +0300

    Создали папку

---
+++ Homework/HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (a > b) max = a;
+if (a > b) min = b;
+if (b > a) max = b;
+if (b > a) min = a;
+
+Console.Write("Большее число равно = ");
+Console.WriteLine(max);
+Console.Write("Меньшее число равно = ");
+Console.WriteLine(min);
