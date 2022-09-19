5073b150ab890a631f1f20873897ad23b247b94a
Elena <helen.ushenko@gmail.com>
2022-09-19 14:22:23 +0300

    Задача 3

---
+++ Homework/HW3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int Num = int.Parse(Console.ReadLine());
+if(Num%2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
