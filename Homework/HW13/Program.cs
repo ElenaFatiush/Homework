7575be4f2ef8b711d6a3c65e54b96138d9d25a5f
Elena <helen.ushenko@gmail.com>
2022-09-22 18:37:44 +0300

    Задача 13

---
+++ Homework/HW13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 100)
+{
+    Console.WriteLine("Число меньше 100");
+    Console.WriteLine($"{number} -> третьей цифры нет"); 
+}
+else
+{
+    int numscreen = number;
+    while(number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    Console.WriteLine($"{numscreen} -> {number}");
+}
