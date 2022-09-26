591f10a381f4f086f10d3b5a4896b9289f8a9062
Elena <helen.ushenko@gmail.com>
2022-09-26 15:39:29 +0300

    ДЗ доп

---
+++ Homework/Sem3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает радиус круга и находит его площадь округленную до целого числа.
+//Необходимо вывести максимальную цифру в полученном округленном значении площади круга
+
+Console.WriteLine("Введите значение радиуса:");
+double r = Convert.ToDouble(Console.ReadLine());
+double Sqr = Math.PI * Math.Pow(r,2);
+Console.WriteLine($"Площадь круга равна {Sqr}");
+double result = Math.Round(Sqr);
+Console.WriteLine(result);
+int n = (int) result;
+int max = 0;
+while (n>0)
+{
+    if (max < n % 10) max = n % 10;
+    n /= 10;
+}
+Console.WriteLine(max);
+
+
+
+
+
